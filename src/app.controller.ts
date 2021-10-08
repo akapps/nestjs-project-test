c22974ef5a70808a9f130eb70f85db9ba1496da4
Antoine Kapps <akapps@digidna.net>
2021-10-08 12:21:03 +0200

    Change endpoint to /health
    - specify path
    - specify returned content-type
    - change service implementation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('health')
+  @Header('Content-Type', 'text/plain')
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getHealthStatus();
   }
 }
